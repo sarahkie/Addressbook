bde05adfa48082bce5f353fd25c006ab42d9227f
Sarah Kieweg <sarah.kieweg@posteo.de>
2026-07-20 10:58:59 +0200

    first commit

---
+++ src/main/java/com/xaidat/praktikum/adressproject/AdressprojectApplication.java
@@ -0,0 +1,13 @@
+package com.xaidat.praktikum.adressproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdressprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdressprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xaidat/praktikum/adressproject/AdressprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xaidat.praktikum.adressproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdressprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
